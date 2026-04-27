652b0d639b68163cfbe3c2f2a2c27bec66483f96
Iriel <irielperezsilva@gmail.com>
2026-04-27 01:44:40 -0300

    Agregar menú principal (feature/menu)

--- src/main/java/app/Program.java
+++ src/main/java/app/Program.java
@@ -3,11 +3,16 @@ package app;
 import data.Persistencia;
 import java.util.InvalidPropertiesFormatException;
 import views.ListarVehiculosView;
+import views.MenuView;
 
 public class Program {
     public static void main(String[] args) throws IllegalArgumentException, InvalidPropertiesFormatException {
         Persistencia.inicializar();
-        ListarVehiculosView view = new ListarVehiculosView();
-        view.setVisible(true);
+        MenuView menu = new MenuView();
+        menu.setVisible(true);
+        
+  
+        
     }
+    
 }
